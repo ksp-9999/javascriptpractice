25d97d7eda60f1fd184c3cc9facc9e8c62ab5955
saiprasadkilaru <kilarusaiprasad19@gmail.com>
2023-11-28 12:12:44 +0530

    arraymethods and fibonacci

---
+++ Arraymethods/first.js
@@ -0,0 +1,16 @@
+let names = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank"];
+// replacing elements from index 2 with the two names
+// console.log(names.splice(2,2,'saiprasad','vamsi'));
+
+// deleting last two elements
+// console.log(names.splice(-2));
+
+// reversing an array
+// console.log(names.reverse());
+
+// sorting an array based on dictionary order.
+// console.log(names.sort())
+
+// sorting an array reverse to the dictionary order.
+// console.log(names.sort((a,b)=>a-b));
+
